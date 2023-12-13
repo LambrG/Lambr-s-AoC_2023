64f2eace379fe581dbe2a07bfa258364a7b471eb
LambrG <jzanat@gmail.com>
2023-12-13 23:33:47 +0100

    2022 update

--- 2022/AoC_22_Day08.py
+++ 2022/AoC_22_Day08.py
@@ -1,12 +1,54 @@
 from file_reader import read_data
-import re
+
 
 raw_data = read_data("2022\input22_08.txt")
+test_data = read_data("2022\\test.txt")
+curr = raw_data
+forrest_map = []
 
-forrest = []
+for line in curr:
+    forrest_map.append([int(x) for x in line.strip()])
+row_len = len(forrest_map[0])
+col_len = len(forrest_map)
 
-for line in raw_data:
-    forrest.append(re.findall("\d", line))
-    
+total = 0
+for row in range(row_len):
+    for col in range(col_len):
+        current = forrest_map[row][col]        
+        left = all(forrest_map[row][x] < current for x in range(col))
+        right = all(forrest_map[row][x] < current for x in range(col+1, col_len))
+        up = all(forrest_map[x][col] < current for x in range(row))
+        down = all(forrest_map[x][col] < current for x in range(row+1, row_len))
+        if any([left, right, up, down]):
+            total += 1
 
-print(forrest)
+print(total)
+
+total = 0
+for row in range(row_len):
+    for col in range(col_len):
+        current = forrest_map[row][col]
+        left = 0
+        for x in range(col-1, -1, -1):
+            left += 1
+            if forrest_map[row][x] >= current:
+                break
+        right = 0
+        for x in range(col+1, col_len):
+            right += 1
+            if forrest_map[row][x] >= current:
+                break
+        up = 0
+        for x in range(row -1, -1, -1):
+            up += 1
+            if forrest_map[x][col] >= current:
+                break 
+        down = 0
+        for x in range(row + 1, row_len):
+            down += 1
+            if forrest_map[x][col] >= current:
+                break
+        total = max(total, left*right*up*down)
+
+
+print(total)
